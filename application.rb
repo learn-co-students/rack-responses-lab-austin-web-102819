b98027a788229555cf34942fafcd56efb217beb6
rachfuerst <rach.fuerst@gmail.com>
2019-11-17 08:45:03 -0600

    Done.

--- application.rb
+++ application.rb
@@ -3,16 +3,14 @@ class Application
         resp= Rack::Response.new
 
         # Get the current time
-        current_time = Time.new
+        linux_time = Time.new
+        current_time= linux_time.strftime("%k:%M") # Return the time without date and other stamps
         # resp.write "#{current_time}" 
         # Check if current_time is greater than or equal to 12
             # If it is, return: "Good Afternoon!"
             # If it is not, return: "Good Morning!"
-        if current_time.hour >= 12
-            resp.write "Good Afternoon!"
-        else
-            resp.write "Good Morning!"
-        end
+        if current_time 
+
         resp.finish
     end
 end
